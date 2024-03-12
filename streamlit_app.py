ffbca38a115424e5e361000db5a5d9d129fcac14
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2024-03-12 11:04:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredientlist:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df = st.dataframe(data= fruityvice_response.json(),use_container_widthTrue)
+fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
     

b7e940ec404b0b14ab34760b9573b36e00b16995
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2024-03-12 11:17:15 +0530

    request fruit details based on the fruit selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ if ingredientlist:
     ingredients_string =''
     for fruit_chosen in ingredientlist:
         ingredients_string +=fruit_chosen+' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information'
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) 

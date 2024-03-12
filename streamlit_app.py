5b5e167f91657538287296b595cc9230b0335567
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2024-03-12 11:45:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
@@ -22,6 +23,7 @@ session = cnx.session()
 #loading the Data frame
 my_dataframe =session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe,use_container_width=True)
+pd_df = my_dataframe.to_pandas()
 
 ingredientlist = st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -32,6 +34,10 @@ if ingredientlist:
     ingredients_string =''
     for fruit_chosen in ingredientlist:
         ingredients_string +=fruit_chosen+' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen,'search_on'].iloc[0]
+        st.write('The search value for',fruit_chosen,' is ', search_on,'.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
